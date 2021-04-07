a6c239697e0f061eb7cc978eff0b3987661265c6
codesquadkiki <47373509+codesquadkiki@users.noreply.github.com>
2021-04-07 18:35:04 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,33 @@
+// START HERE
+
+/* 1.1) Set the first <li> item from the html and print the text from it. 
+    Tip: Use console.log() to print result  
+*/
+
+
+
+/* 1.2) Add a full-stop at the end of each item in all the bucket lists.
+    Hint 1: Select All <li> elements.
+    Hint 2: Use for loop.
+    Hint 3: You will need to use .innerText
+*/
+
+
+
+/* 1.3) Print the first list item from Gandalf's bucket list. 
+    Hint 1: First select the div associated with Gandalf's bucket list.
+*/
+
+
+
+/* 1.4) Change the color of first list item from Gandalf's bucket list to Red.
+    Hint 1: You have already selected this list element from previous part.
+*/
+
+
+
+
+/* 1.5) Remove the first item from Frodo's bucket list.
+    Hint 1: First select the item
+    Hint 2: Use .remove() 
+*/
